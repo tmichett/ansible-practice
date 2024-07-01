98dcb64caabec6b35dbc1b97c331daaa0fbc731e
Travis Michette <tmichett@redhat.com>
2024-07-01 10:29:54 -0400

    Updated the Setup SSH Config Script and Containerfile for M-series Macs

--- Setup_SSH_Config.sh
+++ Setup_SSH_Config.sh
@@ -2,8 +2,22 @@
 # Travis Michette <tmichett@redhat.com>
 
 ## Create SSH Config Drop-In Directory and Copy SSH Config File for Ansible
+printf '\e[1;34m%-6s\e[m' "================================================================"
+echo ""
+printf '\e[1;34m%-6s\e[m' "Creating SSH Client Configuration Files/Directories now ....."
+echo ""
+printf '\e[1;34m%-6s\e[m' "================================================================"
+echo ""
+
 mkdir ~/.ssh/config.d
 cp SSH_Config/SSH_Ansible.conf  ~/.ssh/config.d/SSH_Ansible.conf
 
 ## Insert Line about Drop-In Directory at the Beginning of the File
 sed -i '1s/^/Include ~\/.ssh\/config.d\/*.conf\n/' ~/.ssh/config
+
+printf '\e[1;34m%-6s\e[m' "================================================================"
+echo ""
+printf '\e[1;34m%-6s\e[m' "SSH Clients have been successfully configured ....."
+echo ""
+printf '\e[1;34m%-6s\e[m' "================================================================"
+echo ""
